7d421b5ff22180d848c6fe70fe40857ed077dd39
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-08-01 21:41:13 +0800

    实现queue的push pop peek方法

--- queue/bytes_queue.go
+++ queue/bytes_queue.go
@@ -1,6 +1,8 @@
 package queue
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+)
 
 const (
 	minimumHeaderSize = 17 // 1 byte blobsize + timestampSizeInBytes + hashSizeInBytes
@@ -9,6 +11,10 @@ const (
 
 type queueError struct {
 	message string
+}
+
+func (e *queueError) Error() string {
+	return e.message
 }
 
 var (
@@ -28,6 +34,23 @@ type BytesQueue struct {
 	rightMargin  int
 	headerBuffer []byte
 	verbose      bool
+}
+
+func getNeededSize(length int) int {
+	var header int
+	switch {
+	case length < 127: // 1<<7-1
+		header = 1
+	case length < 16382: // 1<<14-2
+		header = 2
+	case length < 2097149: // 1<<21 -3
+		header = 3
+	case length < 268435452: // 1<<28 -4
+		header = 4
+	default:
+		header = 5
+	}
+	return length + header
 }
 
 func NewBytesQueue(capacity int, maxCapacity int, verbose bool) *BytesQueue {
@@ -50,4 +73,72 @@ func (q *BytesQueue) Reset() {
 	q.rightMargin = leftMarginIndex
 	q.count = 0
 	q.full = false
+}
+
+func (q *BytesQueue) push(data []byte, len int) {
+	headerEntrySize := binary.PutUvarint(q.headerBuffer, uint64(len))
+	q.copy(q.headerBuffer, headerEntrySize)
+
+	q.copy(data, len-headerEntrySize)
+
+	if q.tail > q.head {
+		q.rightMargin = q.tail
+	}
+	if q.tail == q.head {
+		q.full = true
+	}
+
+	q.count++
+}
+
+func (q *BytesQueue) copy(data []byte, len int) {
+	q.tail += copy(q.array[q.tail:], data[:len])
+}
+
+func (q *BytesQueue) Pop() ([]byte, error) {
+	data, blockSize, err := q.peek(q.head)
+	if err != nil {
+		return nil, err
+	}
+
+	q.head += blockSize
+	q.count--
+
+	if q.head == q.rightMargin {
+		q.head = leftMarginIndex
+		if q.tail == q.rightMargin {
+			q.tail = leftMarginIndex
+		}
+		q.rightMargin = q.tail
+	}
+
+	q.full = false
+
+	return data, nil
+}
+
+func (q *BytesQueue) peek(index int) ([]byte, int, error) {
+	err := q.peekCheckErr(index)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	blockSize, n := binary.Uvarint(q.array[index:])
+	return q.array[index+n : index+int(blockSize)], int(blockSize), nil
+}
+
+func (q *BytesQueue) peekCheckErr(index int) error {
+
+	if q.count == 0 {
+		return errEmptyQueue
+	}
+
+	if index <= 0 {
+		return errInvalidIndex
+	}
+
+	if index >= len(q.array) {
+		return errIndexOutOfBounds
+	}
+	return nil
 }
