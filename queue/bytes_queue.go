587184f3ddb3699b8e31adfed6750b1337ab2cc7
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-08-01 21:39:03 +0800

    实现queue数据结构 增加初始化和重置方法

---
+++ queue/bytes_queue.go
@@ -0,0 +1,53 @@
+package queue
+
+import "encoding/binary"
+
+const (
+	minimumHeaderSize = 17 // 1 byte blobsize + timestampSizeInBytes + hashSizeInBytes
+	leftMarginIndex   = 1
+)
+
+type queueError struct {
+	message string
+}
+
+var (
+	errEmptyQueue       = &queueError{"Empty queue"}
+	errInvalidIndex     = &queueError{"Index must be greater than zero. Invalid index."}
+	errIndexOutOfBounds = &queueError{"Index out of range"}
+)
+
+type BytesQueue struct {
+	full         bool
+	array        []byte
+	capacity     int
+	maxCapacity  int
+	head         int
+	tail         int
+	count        int
+	rightMargin  int
+	headerBuffer []byte
+	verbose      bool
+}
+
+func NewBytesQueue(capacity int, maxCapacity int, verbose bool) *BytesQueue {
+	return &BytesQueue{
+		array:        make([]byte, capacity),
+		capacity:     capacity,
+		maxCapacity:  maxCapacity,
+		headerBuffer: make([]byte, binary.MaxVarintLen32),
+		tail:         leftMarginIndex,
+		head:         leftMarginIndex,
+		rightMargin:  leftMarginIndex,
+		verbose:      verbose,
+	}
+}
+
+func (q *BytesQueue) Reset() {
+	// Just reset indexes
+	q.tail = leftMarginIndex
+	q.head = leftMarginIndex
+	q.rightMargin = leftMarginIndex
+	q.count = 0
+	q.full = false
+}
