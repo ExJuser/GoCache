56084cc487cc77757ae10b91a7e24abdb4754d7a
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-08-04 14:33:35 +0800

    完成获取缓存、查询缓存、删除缓存的路由处理方法

--- server/server.go
+++ server/server.go
@@ -5,9 +5,12 @@ import (
 	"flag"
 	"github.com/ExJuser/GoCache"
 	"github.com/gin-gonic/gin"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -58,15 +61,10 @@ func main() {
 		c.Next()
 		logger.Printf("%s request to %s took %vns.", c.Request.Method, c.Request.URL, time.Since(start).Nanoseconds())
 	})
-	r.GET("/api/v1/cache/:key", func(c *gin.Context) {
 
-	})
-	r.PUT("/api/v1/cache/:key", func(c *gin.Context) {
-
-	})
-	r.DELETE("/api/v1/cache/:key", func(c *gin.Context) {
-
-	})
+	r.GET("/api/v1/cache/:key", getCacheHandler)
+	r.PUT("/api/v1/cache/:key", putCacheHandler)
+	r.DELETE("/api/v1/cache/:key", deleteCacheHandler)
 	r.GET("/api/v1/stats", func(c *gin.Context) {
 
 	})
@@ -76,4 +74,78 @@ func main() {
 	if err = r.Run(strPort); err != nil {
 		logger.Print("server start failed.")
 	}
+}
+
+func getCacheHandler(ctx *gin.Context) {
+	target := ctx.Param("key")
+	entry, err := cache.Get(target)
+	if err != nil {
+		errMsg := (err).Error()
+		if strings.Contains(errMsg, "not found") {
+			log.Print(err)
+			ctx.JSON(http.StatusOK, gin.H{
+				"code":  http.StatusNotFound,
+				"value": "缓存不存在",
+			})
+			return
+		}
+		log.Print(err)
+		ctx.JSON(http.StatusOK, gin.H{
+			"code":  http.StatusInternalServerError,
+			"value": "服务器内部错误",
+		})
+		return
+	}
+	log.Printf("成功读取缓存：\"%s\":\"%s\"", target, string(entry))
+	ctx.JSON(http.StatusOK, gin.H{
+		"code":  http.StatusOK,
+		"value": string(entry),
+	})
+}
+
+func putCacheHandler(ctx *gin.Context) {
+	target := ctx.Param("key")
+	entry, err := io.ReadAll(ctx.Request.Body)
+	if err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusOK, gin.H{
+			"code":  http.StatusInternalServerError,
+			"value": "服务器内部错误",
+		})
+	}
+	if err = cache.Set(target, entry); err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusOK, gin.H{
+			"code":  http.StatusInternalServerError,
+			"value": "服务器内部错误",
+		})
+	}
+	log.Printf("成功存储\"%s\":\"%s\"", target, string(entry))
+	ctx.JSON(http.StatusOK, gin.H{
+		"code":  http.StatusCreated,
+		"value": "缓存设置成功",
+	})
+}
+
+func deleteCacheHandler(ctx *gin.Context) {
+	target := ctx.Param("key")
+	if err := cache.Delete(target); err != nil {
+		if strings.Contains((err).Error(), "not found") {
+			ctx.JSON(http.StatusOK, gin.H{
+				"code":  http.StatusNotFound,
+				"value": "缓存不存在",
+			})
+			log.Printf("%s不存在", target)
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"code":  http.StatusInternalServerError,
+			"value": "服务器内部错误",
+		})
+		log.Printf("服务器内部错误：%s", err)
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"code":  http.StatusOK,
+		"value": "缓存删除成功",
+	})
 }
